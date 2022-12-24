640de2aeca475b3a174653e60aa167771b2368af
7696122 <7696122@gmail.com>
2022-12-24 14:33:04 +0900

    up

--- sw.js
+++ sw.js
@@ -83,8 +83,10 @@ self.addEventListener("install", (event) => {
 const cacheFirst = async (request) => {
   const responseFromCache = await caches.match(request);
   if (responseFromCache) {
-    return responseFromCache;
+    if (responseFromCache) return responseFromCache;
   }
+  const response = await request.preloadResponse;
+  if (response) return response;
   return fetch(request);
 };
 
