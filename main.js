f3881ab81801059136843384a139c93d7ce342c4
7696122 <7696122@gmail.com>
2024-09-29 10:53:05 +0900

    .

---
+++ main.js
@@ -0,0 +1,27 @@
+import "https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"
+
+
+// const template = document
+//       .createElement("template");
+// template.innerHTML = `<style="display:flex;">aaa</>`;
+
+// class NavMenuItem extends HTMLElement {
+//   static define (tag = 'nav-menu') {
+//     customElements.define(tag, this)
+//   }
+
+//   constructor() {
+//     super();
+//   }
+//   // Element functionality written in here
+//   connectedCallback() {
+//     this.attachShadow({ mode: "open" });
+//     this.shadowRoot.appendChild(template.content.cloneNode(true));
+//   }
+// }
+
+// window.customElements.define(
+//   "nav-menu",
+//   NavMenuItem,
+//   { extends: "li" }
+// );
